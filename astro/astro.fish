5f4cfc9423f98d1cea42f21842ce292624c7c456
Mubtasim Fuad <m.fuad.rafi@gmail.com>
2024-10-01 11:21:14 +0600

    Merge and update functions

--- astro/astro.fish
+++ astro/astro.fish
@@ -1,6 +1,6 @@
-set -x LD_LIBRARY_PATH /home/rafi/Documents/astrophysics/hdf5-1.14.3/build/lib $LD_LIBRARY_PATH
-
 # For Gevolution & Screening
+
+set -x LD_LIBRARY_PATH /home/rafi/Documents/astrophysics/hdf5-1.14.3/build/lib $LD_LIBRARY_PATH
 
 function run_gevolution
     set start $argv[1]
@@ -24,59 +24,53 @@ function run_screening
     end
 end
 
+function set_mode
+    set short_mode $argv[1]  # 'g' for Gevolution or 's' for Screening
+
+    # Set $mode based on $short_mode
+    if test $short_mode = "g"
+        set -g mode "gevolution"
+    else if test $short_mode = "s"
+        set -g mode "screening"
+    else
+        echo "Invalid mode! Use 'g' for Gevolution or 's' for Screening."
+        return 1
+    end
+
+    # Capitalize the first letter of $mode and store in $Mode
+    set -g Mode (string sub -s 1 -l 1 $mode | string upper)(string sub -s 2 $mode)
+end
+
 #########################################
 
 # For Rockstar
-function run_rockstar_g
-    set start $argv[1]
-    set end $argv[2]
+function run_rockstar
+    set_mode $argv[1]  # 'g' for Gevolution or 's' for Screening
+    set start $argv[2]
+    set end $argv[3]
+    
     for i in (seq -f "%02g" $start $end)
         echo "================================"
-        # rockstar_g
-        echo "Running Rockstar for output_$i... [Gevolution]"
-        python ~/my-scripts/astro/rockstar.py custom_quickstart.cfg /mnt/ssd-ext/gevolution-phi/outputs/output_$i/lcdm_snap003_cdm /mnt/ssd-ext/gevolution-phi/rockstar-outputs/output_$i/
+        # rockstar
+        echo "Running Rockstar for output_$i... [$Mode]"
+        python ~/my-scripts/astro/rockstar.py custom_quickstart.cfg /mnt/ssd-ext/{$mode}-phi/outputs/output_$i/lcdm_snap003_cdm /mnt/ssd-ext/{$mode}-phi/rockstar-outputs/output_$i/
         echo "================================"
-        # halo_pos_mass_g
-        echo "Getting halo position and mass for output_$i... [Gevolution]"
-        python ~/my-scripts/astro/halo-pos-mass.py /mnt/ssd-ext/gevolution-phi/rockstar-outputs/output_$i/halos_0.0.ascii 
+        # halo_pos_mass
+        echo "Getting halo position and mass for output_$i... [$Mode]"
+        python ~/my-scripts/astro/halo-pos-mass.py /mnt/ssd-ext/{$mode}-phi/rockstar-outputs/output_$i/halos_0.0.ascii 
         echo "================================"
     end
 end
 
-function run_rockstar_s
-    set start $argv[1]
-    set end $argv[2]
+function get_halo_pos_mass
+    set_mode $argv[1]  # 'g' for Gevolution or 's' for Screening
+    set start $argv[2]
+    set end $argv[3]
+    
     for i in (seq -f "%02g" $start $end)
         echo "================================"
-        # rockstar_s
-        echo "Running Rockstar for output_$i... [Screening]"
-        python ~/my-scripts/astro/rockstar.py custom_quickstart.cfg /mnt/ssd-ext/screening-phi/outputs/output_$i/lcdm_snap003_cdm /mnt/ssd-ext/screening-phi/rockstar-outputs/output_$i/
-        echo "================================"
-        # halo_pos_mass_g
-        echo "Getting halo position and mass for output_$i... [Screening]"
-        python ~/my-scripts/astro/halo-pos-mass.py /mnt/ssd-ext/screening-phi/rockstar-outputs/output_$i/halos_0.0.ascii 
-        echo "================================"
-    end
-end
-
-function get_halo_pos_mass_g 
-    set start $argv[1]
-    set end $argv[2]
-    for i in (seq -f "%02g" $start $end)
-        echo "================================"
-        echo "Getting halo position and mass for output_$i... [Gevolution]"
-        python ~/my-scripts/astro/halo-pos-mass.py /mnt/ssd-ext/gevolution-phi/rockstar-outputs/output_$i/halos_0.0.ascii 
-        echo "================================"
-    end
-end
-
-function get_halo_pos_mass_s 
-    set start $argv[1]
-    set end $argv[2]
-    for i in (seq -f "%02g" $start $end)
-        echo "================================"
-        echo "Getting halo position and mass for output_$i... [Screening]"
-        python ~/my-scripts/astro/halo-pos-mass.py /mnt/ssd-ext/screening-phi/rockstar-outputs/output_$i/halos_0.0.ascii 
+        echo "Getting halo position and mass for output_$i... [$Mode]"
+        python ~/my-scripts/astro/halo-pos-mass.py /mnt/ssd-ext/{$mode}-phi/rockstar-outputs/output_$i/halos_0.0.ascii 
         echo "================================"
     end
 end
@@ -84,56 +78,33 @@ end
 #########################################
 
 # For Revolver
-function run_revolver_g
-    set start $argv[1]
-    set end $argv[2]
+function run_revolver
+    set_mode $argv[1]  # 'g' for Gevolution or 's' for Screening
+    set start $argv[2]
+    set end $argv[3]
+
     for i in (seq -f "%02g" $start $end)
         echo "================================"
-        # revolver_g
-        echo "Running Revolver for halo_pos_$i... [Gevolution]"
-        TRACER_FILE=/mnt/ssd-ext/gevolution-phi/rockstar-outputs/output_$i/halo_pos_$i.npy python revolver.py --par parameters/custom_params.py
+        # revolver
+        echo "Running Revolver for halo_pos_$i... [$Mode]"
+        TRACER_FILE=/mnt/ssd-ext/{$mode}-phi/rockstar-outputs/output_$i/halo_pos_$i.npy python revolver.py --par parameters/custom_params.py
         echo "================================"
-        # void_pos_radii_g
-        echo "Getting void position and radii for output_$i... [Gevolution]"
-        python ~/my-scripts/astro/void-pos-radii.py /mnt/ssd-ext/gevolution-phi/revolver-outputs/output_$i/zobov-voids_halo_{$i}_cat.txt
+        # void_pos_radii
+        echo "Getting void position and radii for output_$i... [$Mode]"
+        python ~/my-scripts/astro/void-pos-radii.py /mnt/ssd-ext/{$mode}-phi/revolver-outputs/output_$i/zobov-voids_halo_{$i}_cat.txt
         echo "================================"
     end
 end
 
-function run_revolver_s
-    set start $argv[1]
-    set end $argv[2]
+function get_void_pos_radii
+    set_mode $argv[1]  # 'g' for Gevolution or 's' for Screening
+    set start $argv[2]
+    set end $argv[3]
+      
     for i in (seq -f "%02g" $start $end)
         echo "================================"
-        # revolver_s
-        echo "Running Revolver for halo_pos_$i... [Screening]"
-        TRACER_FILE=/mnt/ssd-ext/screening-phi/rockstar-outputs/output_$i/halo_pos_$i.npy python revolver.py --par parameters/custom_params.py
-        echo "================================"
-        # get_void_pos_radii_s
-        echo "Getting void position and radii for output_$i... [Screening]"
-        python ~/my-scripts/astro/void-pos-radii.py /mnt/ssd-ext/screening-phi/revolver-outputs/output_$i/zobov-voids_halo_{$i}_cat.txt
-        echo "================================"
-    end
-end
-
-function get_void_pos_radii_g 
-    set start $argv[1]
-    set end $argv[2]
-    for i in (seq -f "%02g" $start $end)
-        echo "================================"
-        echo "Getting void position and radii for output_$i... [Gevolution]"
-        python ~/my-scripts/astro/void-pos-radii.py /mnt/ssd-ext/gevolution-phi/revolver-outputs/output_$i/zobov-voids_halo_{$i}_cat.txt
-        echo "================================"
-    end
-end
-
-function get_void_pos_radii_s 
-    set start $argv[1]
-    set end $argv[2]
-    for i in (seq -f "%02g" $start $end)
-        echo "================================"
-        echo "Getting void position and radii for output_$i... [Screening]"
-        python ~/my-scripts/astro/void-pos-radii.py /mnt/ssd-ext/screening-phi/revolver-outputs/output_$i/zobov-voids_halo_{$i}_cat.txt
+        echo "Getting void position and radii for output_$i... [$Mode]"
+        python ~/my-scripts/astro/void-pos-radii.py /mnt/ssd-ext/{$mode}-phi/revolver-outputs/output_$i/zobov-voids_halo_{$i}_cat.txt
         echo "================================"
     end
 end
